70f7f52df70a9020b1f58275677a27b96b3be7a2
Paulo2991 <paulovaz38034@gmail.com>
2023-12-14 14:24:09 -0300

    Projeto Spring

---
+++ src/main/java/com/example/sandanter/DIO/SandanterDioApplication.java
@@ -0,0 +1,13 @@
+package com.example.sandanter.DIO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandanterDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandanterDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sandanter/DIO/SandanterDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sandanter.DIO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandanterDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
